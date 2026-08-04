a981b33689c94713ea45b7e2a9b2c4cf6c7057c7
Sardar balnour <balnour.sardar@gmail.com>
2026-08-04 17:22:27 +0330

    configure apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://eu-west-2.cdn.hygraph.com/content/cms328qt502m207w7v9tezt9k/master",
+  }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+  // </StrictMode>
+);

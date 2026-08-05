7bbd9a140ededfa3c393eb068653e83c632654b4
Sardar balnour <balnour.sardar@gmail.com>
2026-08-05 22:46:25 +0330

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Header from "./components/layout/Header";
 import HomePage from "./components/home/HomePage";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-      <Header />
-      <HomePage />
+      <Layout>
+        <HomePage />
+      </Layout>
     </>
   );
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <footer>
+      <Typography
+        component="p"
+        variant="p"
+        // color="primary"
+        sx={{
+          backgroundColor: "#f7f7f7",
+          padding: "10px",
+          textAlign: "center",
+          mt: 10,
+        }}
+      >
+        دوره ریکت | پروژه وبلاگ با GraphQL
+      </Typography>
+    </footer>
+  );
+}
+
+export default Footer;

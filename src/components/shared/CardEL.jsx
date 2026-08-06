fd9d49625611bf50dc1277dca0f6477b7af8dd9d
Sardar balnour <balnour.sardar@gmail.com>
2026-08-06 16:22:41 +0330

    CardEL

--- src/components/shared/CardEL.jsx
+++ src/components/shared/CardEL.jsx
@@ -1,9 +1,60 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
-function CardEL(props) {
-    console.log(props.title)
+function CardEL({ title, slug, coverPhoto, author }) {
   return (
-    <div>CardEL</div>
-  )
+    <Card sx={{ boxShadow: "rgba(0, 0, 0, 0.1)0px 4px 12px", borderRadius: 4 }}>
+      <CardHeader
+        avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
+        title={
+          <Typography component="p" variant="p" color="text.secondary">
+            {author.name}
+          </Typography>
+        }
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={coverPhoto.url}
+        alt={slug}
+      />
+      <CardContent>
+        <Typography
+          component="h3"
+          variant="h6"
+          color="text.primary"
+          sx={{ fontWeight: 600, minHeight: 95, marginTop: 2 }}
+        >
+          {title}
+        </Typography>
+      </CardContent>
+      <Divider variant="middle" sx={{ margin: "10px" }} />
+      <CardActions>
+        {/* <Link
+          to={`/blogs/${slug}`}
+          style={{ textDecoration: "none", width: "100%" }}
+        > */}
+          <Button
+            variant="outlined"
+            size="small"
+            sx={{ width: "100%", fontWeight: 600 }}
+          >
+            مطالعه مقاله
+          </Button>
+        {/* </Link> */}
+      </CardActions>
+    </Card>
+  );
 }
 
-export default CardEL
+export default CardEL;

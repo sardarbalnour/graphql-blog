25579fa2c1ab6acb4e9a5ecf74eb23ee32479645
Sardar balnour <balnour.sardar@gmail.com>
2026-08-06 17:34:49 +0330

    Authors

--- src/components/author/Authors.jsx
+++ src/components/author/Authors.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import { useQuery } from "@apollo/client/react";
-import { Avatar, Grid, Typography } from "@mui/material";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
 
 import { GET_AUTHORS_INFO } from "../../graphql/queries";
 
 function Authors() {
   const { loading, data, error } = useQuery(GET_AUTHORS_INFO);
-
   if (loading) return <h3>Loading...</h3>;
   if (error) return <h4>Something went wrong! {error.message}</h4>;
-
+  
+  const { authors } = data;
   console.log({ loading, data, error });
 
   return (
@@ -16,22 +17,29 @@ function Authors() {
       container
       sx={{ boxShadow: "rgba(0, 0, 0, 0.1)0px 4px 12px", borderRadius: 4 }}
     >
-      {data.authors.map((author) => (
-        <Grid xs={12} sx={{ padding: 2 }} key={author.id}>
-          <a
-            href={`/authors/${author.slug}`}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              textDecoration: "none",
-            }}
-          >
-            <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
-            <Typography component="p" variant="p" color="text.secondary">
-              {author.name}
-            </Typography>
-          </a>
-        </Grid>
+      {authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid size={12} sx={{ padding: 2 }}>
+            <a
+              href={`/authors/${author.slug}`}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+              }}
+            >
+              <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
+              <Typography component="p" variant="p" color="text.secondary">
+                {author.name}
+              </Typography>
+            </a>
+          </Grid>
+          {index !== authors.length - 1 && (
+            <Grid size={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
       ))}
     </Grid>
   );

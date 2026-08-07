282f7e99758454a4af3618a4ad16693e610dd4b9
Sardar balnour <balnour.sardar@gmail.com>
2026-08-07 15:22:39 +0330

    AuthorPage dynamic data

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client/react";
 import { useParams } from "react-router-dom";
 
 import { GET_AUTHOR_INFO } from "../../graphql/queries";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
 
 function AuthorPage() {
   const { slug } = useParams();
@@ -13,7 +14,41 @@ function AuthorPage() {
   if (error) return <h4>Something went wrong! {error.message}</h4>;
   console.log(data);
 
-  return <div>AuthorPage</div>;
+  const {
+    author: { name, avatar, field, description },
+  } = data;
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container sx={{ mt: 10 }}>
+        <Grid
+          size={12}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar src={avatar.url} sx={{ width: 250, height: 250 }} />
+          <Typography
+            component="h3"
+            variant="h5"
+            sx={{ fontWeight: 700, mt: 4 }}
+          >
+            {name}
+          </Typography>
+          <Typography
+            component="p"
+            variant="h5"
+            sx={{ mt: 2, color: "text.secondary" }}
+          >
+            {field}
+          </Typography>
+        </Grid>
+        <Grid size={12}>{description.html}</Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default AuthorPage;

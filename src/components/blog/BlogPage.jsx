e280f4170330961b69d548cbf8c9cc6e93be8779
Sardar balnour <balnour.sardar@gmail.com>
2026-08-08 10:50:42 +0330

    GET_POST_INFO

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,8 +1,20 @@
+import { useQuery } from "@apollo/client/react";
+import { useParams } from "react-router-dom";
+
+import { GET_POST_INFO } from "../../graphql/queries";
+import Loader from "../shared/Loader";
 
 function BlogPage() {
-  return (
-    <div>BlogPage</div>
-  )
+  const { slug } = useParams();
+
+  const { loading, data, error } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+  if (loading) return <Loader />;
+  if (error) return <h4>Something went wrong! {error.message}</h4>;
+  console.log({ loading, data, error });
+
+  return <div>BlogPage</div>;
 }
 
-export default BlogPage
+export default BlogPage;
